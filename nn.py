e9c62b9faea545cdcca82eda79bc7d66bfc51fc2
Callum Yarnold <callum@yarnold.me>
2020-12-19 17:51:35 +0000

    Added softmax activation function and removed vscode

--- nn.py
+++ nn.py
@@ -1,29 +1,40 @@
+#!/usr/bin/python
 import numpy as np
 
 import nnfs
 from nnfs.datasets import spiral_data
 nnfs.init()
 
-X, y = spiral_data(100, 3)
-
 class Layer_Dense:
-	def __init__(self, n_inputs, n_neurons):
-		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
-		self.biases = np.zeros((1, n_neurons))
+  def __init__(self, n_inputs, n_neurons):
+    self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+    self.biases = np.zeros((1, n_neurons))
 
-	def forward(self, inputs):
-		self.output = np.dot(inputs, self.weights) + self.biases
+  def forward(self, inputs):
+    self.output = np.dot(inputs, self.weights) + self.biases
 
 class Activation_ReLU:
-	def forward(self, inputs):
-		self.output = np.maximum(0, inputs)
+  def forward(self, inputs):
+    self.output = np.maximum(0, inputs)
 
+class Activation_Softmax:
+  def forward(self, inputs):
+    exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+    probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+    self.output = probabilities
 
+x, y = spiral_data(samples=100, classes=3)
 
-layer1 = Layer_Dense(2, 5)
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(x)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
